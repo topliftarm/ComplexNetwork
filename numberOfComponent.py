349b9b4382a45824d30dd556faaac933077d7c7f
vahid <topliftarm@gmail.com>
2019-07-05 23:21:42 +0430

    number of component added

---
+++ numberOfComponent.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import networkx as nx 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import sys
+
+mpl.rcParams['agg.path.chunksize'] = 10000
+
+
+dirName = sys.argv[1]
+
+if (len(sys.argv) == 1):
+    print("Please Insert Dir. Name")
+    sys.exit(1)
+elif (len(sys.argv)==2):
+    print("argv==2")
+    dirPath = "/home/vahid/Documents/Complex network/c/CompleteData"
+elif (len(sys.argv)==3):
+    print("argv==3")
+    dirPath = sys.argv[2]
+
+
+	
+DirPath1 = dirPath+'/'+dirName
+fileName_adj = 'FinalAdj.txt' 
+
+Adj = np.loadtxt(DirPath1+'/'+fileName_adj).reshape(-1, 100*100).ravel().tolist() 
+init_adj_mat = np.asarray(Adj).reshape((100,100))  
+G=nx.from_numpy_matrix(init_adj_mat) 
+L = nx.normalized_laplacian_matrix(G).todense() 
+a = np.linalg.eig(L) 
+
+print(a)
